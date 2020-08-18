cec73705ca9c58b5dfa72557709ab0661d2df3b7
mocory <mocoryg@gmail.com>
2020-08-18 10:17:43 +0900

    基礎プログラムを追加

---
+++ Assets/Script/Handchecker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Handchecker : MonoBehaviour
+{
+    public OVRSkeleton _skeleton; //右手、もしくは左手の Bone情報
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var isIndexStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
+        var isMiddleStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
+        var isRingStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Ring1, OVRSkeleton.BoneId.Hand_Ring2, OVRSkeleton.BoneId.Hand_Ring3, OVRSkeleton.BoneId.Hand_RingTip);
+        var isPinkyStraight = IsStraight(0.8f, OVRSkeleton.BoneId.Hand_Pinky0, OVRSkeleton.BoneId.Hand_Pinky1, OVRSkeleton.BoneId.Hand_Pinky2, OVRSkeleton.BoneId.Hand_Pinky3, OVRSkeleton.BoneId.Hand_PinkyTip);
+
+        Debug.Log("Index is " + isIndexStraight);
+        Debug.Log("Middle is " + isIndexStraight);
+        Debug.Log("Ring is " + isIndexStraight);
+        Debug.Log("Pinky is " + isIndexStraight);
+
+        text.text = "Index is " + isIndexStraight.ToString();
+    }
+    private bool IsStraight(float threshold, params OVRSkeleton.BoneId[] boneids)
+    {
+        if (boneids.Length < 3) return false;   //調べようがない
+        Vector3? oldVec = null;
+        var dot = 1.0f;
+        for (var index = 0; index < boneids.Length - 1; index++)
+        {
+            var v = (_skeleton.Bones[(int)boneids[index + 1]].Transform.position - _skeleton.Bones[(int)boneids[index]].Transform.position).normalized;
+            if (oldVec.HasValue)
+            {
+                dot *= Vector3.Dot(v, oldVec.Value); //内積の値を総乗していく
+            }
+            oldVec = v;//ひとつ前の指ベクトル
+        }
+        return dot >= threshold; //指定したBoneIDの内積の総乗が閾値を超えていたら直線とみなす
+    }
+}
