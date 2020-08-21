0fd0188a452cbf54195c96ad3f34ed8bf5cc0dc7
momiji cory <mocoryg@gmail.com>
2020-08-21 19:11:17 +0900

    未実装要素を追加

--- Assets/Script/Handchecker.cs
+++ Assets/Script/Handchecker.cs
@@ -8,38 +8,34 @@ public class Handchecker : MonoBehaviour
     public Transform HandR, HandL,Head;
     public OVRSkeleton _skeletonR,_skeletonL; //右手、もしくは左手の Bone情報
     public Text textR,textL;
-    public int HandrotateR, HandrotateL;
+    public bool isIndexStraight, isMiddleStraight, isRingStraight, isPinkyStraight, isThumbStraight, isIndexStraightL, isMiddleStraightL, isRingStraightL, isPinkyStraightL, isThumbStraightL;
+    public bool[] FingerR, FingerL;
+    public int HandrotateR, HandrotateL, HandposVR, HandposVL, HandposHR, HandposHL;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        var isIndexStraight = IsStraight(true,0.8f, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
-        var isMiddleStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
-        var isRingStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Ring1, OVRSkeleton.BoneId.Hand_Ring2, OVRSkeleton.BoneId.Hand_Ring3, OVRSkeleton.BoneId.Hand_RingTip);
-        var isPinkyStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Pinky0, OVRSkeleton.BoneId.Hand_Pinky1, OVRSkeleton.BoneId.Hand_Pinky2, OVRSkeleton.BoneId.Hand_Pinky3, OVRSkeleton.BoneId.Hand_PinkyTip);
-        var isThumbStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Thumb1, OVRSkeleton.BoneId.Hand_Thumb2, OVRSkeleton.BoneId.Hand_Thumb3, OVRSkeleton.BoneId.Hand_ThumbTip);
+        isThumbStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Thumb1, OVRSkeleton.BoneId.Hand_Thumb2, OVRSkeleton.BoneId.Hand_Thumb3, OVRSkeleton.BoneId.Hand_ThumbTip);
+        isIndexStraight = IsStraight(true,0.8f, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
+        isMiddleStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
+        isRingStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Ring1, OVRSkeleton.BoneId.Hand_Ring2, OVRSkeleton.BoneId.Hand_Ring3, OVRSkeleton.BoneId.Hand_RingTip);
+        isPinkyStraight = IsStraight(true, 0.8f, OVRSkeleton.BoneId.Hand_Pinky0, OVRSkeleton.BoneId.Hand_Pinky1, OVRSkeleton.BoneId.Hand_Pinky2, OVRSkeleton.BoneId.Hand_Pinky3, OVRSkeleton.BoneId.Hand_PinkyTip);
+        FingerR= new bool[] { isThumbStraight,isIndexStraight,isMiddleStraight,isRingStraight,isPinkyStraight };
 
-        var isIndexStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
-        var isMiddleStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
-        var isRingStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Ring1, OVRSkeleton.BoneId.Hand_Ring2, OVRSkeleton.BoneId.Hand_Ring3, OVRSkeleton.BoneId.Hand_RingTip);
-        var isPinkyStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Pinky0, OVRSkeleton.BoneId.Hand_Pinky1, OVRSkeleton.BoneId.Hand_Pinky2, OVRSkeleton.BoneId.Hand_Pinky3, OVRSkeleton.BoneId.Hand_PinkyTip);
-        var isThumbStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Thumb1, OVRSkeleton.BoneId.Hand_Thumb2, OVRSkeleton.BoneId.Hand_Thumb3, OVRSkeleton.BoneId.Hand_ThumbTip);
+        isThumbStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Thumb1, OVRSkeleton.BoneId.Hand_Thumb2, OVRSkeleton.BoneId.Hand_Thumb3, OVRSkeleton.BoneId.Hand_ThumbTip);
+        isIndexStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Index1, OVRSkeleton.BoneId.Hand_Index2, OVRSkeleton.BoneId.Hand_Index3, OVRSkeleton.BoneId.Hand_IndexTip);
+        isMiddleStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Middle1, OVRSkeleton.BoneId.Hand_Middle2, OVRSkeleton.BoneId.Hand_Middle3, OVRSkeleton.BoneId.Hand_MiddleTip);
+        isRingStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Ring1, OVRSkeleton.BoneId.Hand_Ring2, OVRSkeleton.BoneId.Hand_Ring3, OVRSkeleton.BoneId.Hand_RingTip);
+        isPinkyStraightL = IsStraight(false, 0.8f, OVRSkeleton.BoneId.Hand_Pinky0, OVRSkeleton.BoneId.Hand_Pinky1, OVRSkeleton.BoneId.Hand_Pinky2, OVRSkeleton.BoneId.Hand_Pinky3, OVRSkeleton.BoneId.Hand_PinkyTip);
+        FingerL = new bool[] { isThumbStraightL, isIndexStraightL, isMiddleStraightL, isRingStraightL, isPinkyStraightL };
 
-        Debug.Log("Index is " + isIndexStraight);
-        Debug.Log("Middle is " + isIndexStraight);
-        Debug.Log("Ring is " + isIndexStraight);
-        Debug.Log("Pinky is " + isIndexStraight);
-        Debug.Log("Thumb is " + isThumbStraight);
-
-
-
-        textR.text = "親指は" + IsFingerstraight(isThumbStraight) + "\n人差し指は" + IsFingerstraight(isIndexStraight) + "\n中指は" + IsFingerstraight(isMiddleStraight) + "\n薬指は" + IsFingerstraight(isRingStraight) + "\n小指は" + IsFingerstraight(isPinkyStraight) + "\n" + Handrot(HandR, true) + "\n" + Head2handposV(HandR) + Head2handposH(HandR);
-        textL.text = "親指は" + IsFingerstraight(isThumbStraightL) + "\n人差し指は " + IsFingerstraight(isIndexStraightL) + "\n中指は" + IsFingerstraight(isMiddleStraightL) + "\n薬指は" + IsFingerstraight(isRingStraightL) + "\n小指は" + IsFingerstraight(isPinkyStraightL) + "\n" + Handrot(HandL, false) + "\n" + Head2handposV(HandL) + Head2handposH(HandL);
+        textR.text = "親指は" + IsFingerstraight(isThumbStraight) + "\n人差し指は" + IsFingerstraight(isIndexStraight) + "\n中指は" + IsFingerstraight(isMiddleStraight) + "\n薬指は" + IsFingerstraight(isRingStraight) + "\n小指は" + IsFingerstraight(isPinkyStraight) + "\n" + Handrot(HandR, true) + "\n" + Head2handposV(HandR,true) + Head2handposH(HandR,true);
+        textL.text = "親指は" + IsFingerstraight(isThumbStraightL) + "\n人差し指は " + IsFingerstraight(isIndexStraightL) + "\n中指は" + IsFingerstraight(isMiddleStraightL) + "\n薬指は" + IsFingerstraight(isRingStraightL) + "\n小指は" + IsFingerstraight(isPinkyStraightL) + "\n" + Handrot(HandL, false) + "\n" + Head2handposV(HandL,false) + Head2handposH(HandL,false);
     }
 
     string IsFingerstraight(bool fingerstraight)
@@ -60,63 +56,116 @@ public class Handchecker : MonoBehaviour
         {
             if (Hand.localEulerAngles.y <= 135 && Hand.localEulerAngles.y >= 45)
             {
+                HandrotateR = 0;
                 return "正面";
             }
             else if ((Hand.localEulerAngles.y < 45 && Hand.localEulerAngles.y >= 0) || (Hand.localEulerAngles.y <= 360 && Hand.localEulerAngles.y >= 315))
             {
+                HandrotateR = 1;
                 return "側面";
             }
             else if (Hand.localEulerAngles.y < 315 && Hand.localEulerAngles.y >= 225)
             {
+                HandrotateR = 2;
                 return "背面";
             }
+            HandrotateR = 3;
             return "エラー";
         }
         else
         {
             if (Hand.localEulerAngles.y <= 135 && Hand.localEulerAngles.y >= 45)
             {
+                HandrotateL = 0;
                 return "正面";
             }
             else if (Hand.localEulerAngles.y <= 225 && Hand.localEulerAngles.y > 135)
             {
+                HandrotateL = 1;
                 return "側面";
             }
             else if (Hand.localEulerAngles.y <= 315 && Hand.localEulerAngles.y > 225)
             {
+                HandrotateL = 2;
                 return "背面";
             }
+            HandrotateL = 3;
             return "エラー";
         }
     }
 
-    string Head2handposV(Transform Hand)
+    string Head2handposV(Transform Hand,bool IsR)
     {
-        if ((Hand.localPosition.y - Head.localPosition.y) <= -0.3f)
+        if (IsR)
         {
-            return "胸の";
+            if ((Hand.localPosition.y - Head.localPosition.y) <= -0.3f)
+            {
+                HandposVR = 0;
+                return "胸の";
+            }
+            else if ((Hand.localPosition.y - Head.localPosition.y) > -0.3f)
+            {
+                HandposVR = 1;
+                return "顔の";
+            }
+            return "エラー";
         }
-        else if ((Hand.localPosition.y - Head.localPosition.y) > -0.3f)
+        else
         {
-            return "顔の";
+            if ((Hand.localPosition.y - Head.localPosition.y) <= -0.3f)
+            {
+                HandposVL = 0;
+                return "胸の";
+            }
+            else if ((Hand.localPosition.y - Head.localPosition.y) > -0.3f)
+            {
+                HandposVL = 1;
+                return "顔の";
+            }
+            return "エラー";
         }
-        return "エラー";
+
     }
-    string Head2handposH (Transform Hand)
+    string Head2handposH (Transform Hand, bool IsR)
     {
-        if ((Hand.localPosition.x - Head.localPosition.x) >= 0.15f)
+        if (IsR)
         {
-            return "右側";
+            if ((Hand.localPosition.x - Head.localPosition.x) >= 0.15f)
+            {
+                HandposHR = 2;
+                return "右側";
+            }
+            else if ((Hand.localPosition.x - Head.localPosition.x) < 0.15f && (Hand.localPosition.x - Head.localPosition.x) > -0.15f)
+            {
+                HandposHR = 1;
+                return "前";
+            }
+            else if ((Hand.localPosition.x - Head.localPosition.x) <= -0.15f)
+            {
+                HandposHR = 0;
+                return "左側";
+            }
+            return "エラー";
         }
-        else if ((Hand.localPosition.x - Head.localPosition.x) < 0.15f && (Hand.localPosition.x - Head.localPosition.x) > -0.15f)
+        else
         {
-            return "前";
+            if ((Hand.localPosition.x - Head.localPosition.x) >= 0.15f)
+            {
+                HandposHL = 2;
+                return "右側";
+            }
+            else if ((Hand.localPosition.x - Head.localPosition.x) < 0.15f && (Hand.localPosition.x - Head.localPosition.x) > -0.15f)
+            {
+                HandposHL = 1;
+                return "前";
+            }
+            else if ((Hand.localPosition.x - Head.localPosition.x) <= -0.15f)
+            {
+                HandposHL = 0;
+                return "左側";
+            }
+            return "エラー";
         }
-        else if ((Hand.localPosition.x - Head.localPosition.x) <= -0.15f)
-        {
-            return "左側";
-        }
-        return "エラー";
     }
     private bool IsStraight(bool isR,float threshold, params OVRSkeleton.BoneId[] boneids)
     {
---
+++ Assets/Script/Handshape.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handshape : MonoBehaviour
+{
+    Handchecker handchecker;
+    public int Step, Fase;
+    public OVRHand HandL, HandR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handchecker = GetComponent<Handchecker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Step)
+        {
+            case 0:
+                Learnstart();
+                break;
+            case 1:
+                Hello();
+                break;
+        }
+    }
+
+    void Learnstart()
+    {
+        if (HandR.GetFingerIsPinching(OVRHand.HandFinger.Index))
+        {
+            Step = 1;
+        }
+        else if (HandR.GetFingerIsPinching(OVRHand.HandFinger.Middle))
+        {
+            Step = 2;
+        }
+        else if (HandR.GetFingerIsPinching(OVRHand.HandFinger.Ring))
+        {
+            Step = 3;
+        }
+    }
+
+    void Hello()
+    {
+        switch (Fase)
+        {
+            case 0:
+                if (Fingermatch(handchecker.FingerR,new bool[] { false, true, true, false, false }))
+                {
+                    Fase++;
+                }
+                break;
+            case 1:
+
+                break;
+        }
+    }
+    bool Fingermatch(bool[] RealFinger,bool[] CorrectFinger)
+    {
+        for(int i = 0; i < RealFinger.Length; i++)
+        {
+            if (RealFinger[i] != CorrectFinger[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
